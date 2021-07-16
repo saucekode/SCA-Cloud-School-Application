3d252a0030f93a7cd14dcf53aaf2dac4521789ec
saucekode <saucekoder@gmail.com>
2021-07-16 12:23:01 +0100

    pushing files to start branch

---
+++ docker/src/main/java/com/app/docker/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.app.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/")
+    public String welcome(){
+        return "Welcome to SCA Cloud School Application";
+    }
+}
